18c37cffb3e5d3c482b8e22b1c2fa87fdb706ecf
Lloyd Taylor <ltaylor@netelder.com>
2013-06-09 09:47:55 -0700

    initial commit

---
+++ app/models/event.rb
@@ -0,0 +1,18 @@
+require 'date_validator'
+
+class Event < ActiveRecord::Base
+
+	validates :date, :title, :organizer_name, :organizer_email, :presence => true
+	validates :title, :uniqueness => true
+	validates_format_of :organizer_email, :with => /@/  # validate on input form, and send double-opt-in email to this address
+	validates_date_of :date, :after => Proc.new { Time.now }
+
+end
+
+
+
+# attributes	
+# t.string :organizer_name
+# t.string :organizer_email
+# t.string :title
+# t.date :date
---
+++ public/js/application.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+  // send an HTTP DELETE request for the sign-out link
+  $('a#sign-out').on("click", function (e) {
+  	e.preventDefault();
+  	var request = $.ajax({ url: $(this).attr('href'), type: 'delete' });
+  	request.done(function () { window.location = "/"; });
+  });
+
+
+
+// Mailcheck js
+
+	$('#email').on('blur', function() {
+		$(this).mailcheck({
+			suggested: function(element, suggestion) {
+	      // callback code
+	 	 	},
+	 		empty: function(element) {
+	      // callback code
+	  		}
+		});
+	});
+
+});
